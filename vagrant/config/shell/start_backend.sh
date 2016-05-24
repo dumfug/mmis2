b8333919ecc26ed0a1c864ca195bb5e5012ea265
Thomas Mauerhofer <thomas.mauerhofer@student.tugraz.at>
2016-05-24 19:57:00 +0200

    Added Template for userinterface

--- vagrant/config/shell/start_backend.sh
+++ vagrant/config/shell/start_backend.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-echo "Start Server on Port:5000"
 
 cd /git_home
+
+echo "Install dependencies"
 sudo pip3 install -r requirements.txt
+
+echo "Start Server on Port:5000"
 python3 timeline/__init__.py > /dev/null 2>&1 &
 
